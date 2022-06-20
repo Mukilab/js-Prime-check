b8deacb3a6432a34f31638de9869b9358360adf6
Mukilab <97949146+Mukilab@users.noreply.github.com>
2022-06-20 08:32:43 +0300

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,10 @@
+function isPrime(num) { 
+    if (num <= 1) return false; 
+    if (num % 2 == 0 && num > 2) return false;
+    const s = Math.sqrt(num); 
+    for(let i = 3; i <= s; i += 2) { 
+        if(num % i === 0) return false; 
+    }
+    return true;
+  }
+  console.log(isPrime(121));
